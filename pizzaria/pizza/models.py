618b04a6ea129d7b3beda59e0215fa5f0e2c2b62
aluno <willian_vidal@hotmail.com>
2015-08-18 15:05:04 -0300

    Pizzaria nova

---
+++ pizzaria/pizza/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=128)
+    price = models.DecimalField(decimal_places=2, max_digits=5)
+    ingredients  = models.TextField()
+
+    def __unicode__(self):
+        return self.name    
